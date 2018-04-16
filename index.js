76e5ecc18eed897d74ea80e9d264aeca2480df0c
Yang Yan <yangawesome@gmail.com>
2018-04-16 15:49:12 -0400

    basic views

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+var mapElement = 'map';
+
 function initMap() {
 	var uluru = {lat: -25.363, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('map'), {
+	var shanghai = {lat: 31.2240453, lng: 121.1965744};
+	var map = new google.maps.Map(document.getElementById(mapElement), {
 		zoom: 4,
 		center: uluru
 	});
 	var marker = new google.maps.Marker({
 		position: uluru,
+		map: map
+	});
+	var marker2 = new google.maps.Marker({
+		position: shanghai,
 		map: map
 	});
 }
