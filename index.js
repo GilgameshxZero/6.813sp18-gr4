25c4a18d80a5da6b7b387f3db1e48085ba4bc92a
ajlee19 <ashleyjlee19@gmail.com>
2018-04-19 05:21:36 -0400

    landing page and map interface

--- index.js
+++ index.js
@@ -1,23 +1,292 @@
 var mapElement = 'map';
-
-function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	var shanghai = {lat: 31.2240453, lng: 121.1965744};
-	var map = new google.maps.Map(document.getElementById(mapElement), {
-		zoom: 4,
-		center: uluru
-	});
-	var marker = new google.maps.Marker({
-		position: uluru,
-		map: map
-	});
-	var marker2 = new google.maps.Marker({
-		position: shanghai,
-		map: map
-	});
-}
+var rotate;
 
 window.onload = function () {
+	var map = document.getElementById("map");
+	map.style.filter = "blur(2px)";
+
+	var suggestions = ["What kind of weather are you looking for?","what kind of activities do you want to do?","Any specific region mind?"];
+	var placeHolder = document.getElementById("input").placeholder;
+	rotate = setInterval(change, 2000);
+	var counter = 0;
+	function change() { 
+		// $('#input').attr('placeholder').fadeOut(400,function(){
+  //     		$(this).attr('placeholder').html(suggestions[counter]).fadeIn();  
+  //   	});
+  		placeHolder = suggestions[counter];
+	  	counter++;
+	  	if (counter >= suggestions.length) {
+			counter = 0;
+	  	}
+	}
+}
+
+function initMap() {
+	var mit = {lat: 42.358792, lng: -71.093493};
+	var florida = {lat: 27.775295, lng: -81.576485};
+	var regensburg = {lat: 49.013454, lng: 12.100383};
+	var bryceCanyon = {lat: 37.593833, lng: -112.187092}
+	// var shanghai = {lat: 31.2240453, lng: 121.1965744};
+	var map = new google.maps.Map(document.getElementById(mapElement), {
+		zoom: 2,
+		minZoom:2.3,
+		center: mit
+	});
+
+	function makePopups(locationName, position_x, position_y){
+		var mapDiv = document.getElementById("map");
+
+		var existingPops = document.getElementsByClassName("popup");
+		for (var i = 0; i < existingPops.length; i++)
+			mapDiv.removeChild(existingPops[i]);
+
+		var popup = document.createElement("div");
+		popup.classList.add("popup");
+		popup.id = locationName;
+		popup.style.top = (position_y - 100) + "px";
+		popup.style.left = (position_x - 100) + "px";
+
+		mapDiv.appendChild(popup);
+	};
+
+	var florida_data = {
+		"name": "florida",
+		"position": florida,
+		"text": ["warm", "beach"],
+		"language": "---",
+		"citizenship": "---",
+		"pets": false,
+		"kids": false,
+		"budget": 0
+	};
+	var marker_florida = new google.maps.Marker({
+		position: florida,
+		map: map
+	});
+	marker_florida.setVisible(false);
+
+	var regensburg_data = {
+		"name": "regensburg",
+		"position": regensburg,
+		"text": ["europe", "oldcities", "castles"],
+		"language": "german",
+		"citizenship": "---",
+		"pets": false,
+		"kids": false,
+		"budget": 200,
+	}
+	var marker_regensburg = new google.maps.Marker({
+		position: regensburg,
+		map: map
+	});
+	marker_regensburg.setVisible(false);
+
+	var bryce_data = {
+		"name": "bryce",
+		"position": bryceCanyon,
+		"text": ["hiking", "camping"],
+		"language": "---",
+		"citizenship": "canada",
+		"pets": true,
+		"kids": true,
+		"budget": 0
+	};
+	var marker_bryce = new google.maps.Marker({
+		position: bryceCanyon,
+		map: map
+	});
+	marker_bryce.setVisible(false);
+
+	var datas = [florida_data, regensburg_data, bryce_data];
+	var markers = {
+		"bryce": marker_bryce, 
+		"florida": marker_florida, 
+		"regensburg": marker_regensburg
+	};
+
+	for (var i = 0; i < datas.length; i++){
+		data = datas[i]["name"];
+		google.maps.event.addListener(markers[data], 'click', function(){
+			makePopups(data, event.clientX, event.clientY);
+		});
+	}
+
+	var inputWrapper = document.getElementById("input-wrapper");
+	var input = document.getElementById("input");
+	input.focus();
+
+	function checkData(){
+		var tags = document.getElementsByClassName("tag");
+    	var allText = [];
+    	for (var i = 0; i < tags.length; i++){
+    		allText.push(tags[i].innerHTML);
+    	}
+    	for (var i = 0; i < datas.length; i++){
+    		var matched = true;
+    		// check for text
+    		dataText = datas[i]["text"];
+    		for (var j = 0; j < dataText.length; j++){
+    			if (allText.indexOf(dataText[j]) < 0){
+    				matched = false;
+    				break;
+    			}
+    		}
+
+    		// language preference
+    		var languageSelect = document.getElementById("rst-language-select");
+			var l_option = languageSelect.options[languageSelect.selectedIndex].text;
+			
+			if (l_option.toLowerCase() != datas[i]['language']){
+				matched = false;
+			}
+
+    		// citizenship preference
+    		var citizenshipSelect = document.getElementById("rst-citizenship-select");
+			var c_option = citizenshipSelect.options[citizenshipSelect.selectedIndex].text;
+			
+			if (c_option.toLowerCase() != datas[i]['citizenship']){
+				matched = false;
+			}
+
+    		// pets preference
+    		petsBool = datas[i]["pets"];
+    		// console.log(document.getElementById("petsCheckbox").checked);
+    		if (petsBool != document.getElementById("petsCheckbox").checked){
+    			matched = false;
+    		}
+
+    		// kids preference
+    		kidsBool = datas[i]["kids"];
+    		if (petsBool != document.getElementById("kidsCheckbox").checked){
+    			matched = false;
+    		}
+
+    		// budget
+    		var currentBudget = parseInt(document.getElementById("rst-price-display-current").innerHTML.slice(1));
+    		if (Math.abs(currentBudget - datas[i]["budget"]) > 50){
+    			matched = false;
+    		}
+
+    		if (matched){
+    			markers[datas[i]["name"]].setVisible(true);
+    		}
+    		else{
+    			markers[datas[i]["name"]].setVisible(false);
+    		}
+    	}
+	}
+	input.addEventListener("keyup", function(event) {
+	    event.preventDefault();
+	    // Make a new timeout set to go off in 800ms
+        if (event.keyCode === 13) {
+	    	var tag = document.createElement("span");
+	    	tag.classList.add("tag");
+	    	// this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,'');
+	        tag.innerHTML = input.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,'');;
+	        inputWrapper.insertBefore(tag, input);
+	        input.value = "";
+
+	    	checkData();
+	    }
+	    // BUG with how to fix when only one word left in input value
+	    else if (event.keyCode === 8 && input.value == "") {
+	    	// console.log(inputWrapper.childNodes);
+	    	var tag = inputWrapper.childNodes[inputWrapper.childNodes.length - 5];
+	    	if (tag){
+		    	inputWrapper.removeChild(tag);
+		    	checkData();
+		    }
+
+    	}
+	});
+
+	inputWrapper.addEventListener("click", function(event){
+		var tag = event.target;
+		if (tag.classList.contains("tag")){
+			var tag = event.target;
+			if (tag.classList.contains("tag")){
+				inputWrapper.removeChild(tag);
+				input.focus();
+			}
+			checkData();
+		}
+	});
+
+	document.getElementById("petsCheckbox")
+		.addEventListener("click", function(event){
+			checkData();
+	});
+
+	document.getElementById("kidsCheckbox")
+		.addEventListener("click", function(event){
+			checkData();
+	});
+
+	document.getElementById("rst-language-select")
+		.addEventListener("change", function() {
+		    checkData();
+	});
+
+	document.getElementById("rst-citizenship-select")
+		.addEventListener("change", function() {
+		    checkData();
+	});
+
+	document.getElementById("rst-price-slider")
+		.addEventListener("change", function() {
+		    checkData();
+	});
+
+	// taking care of centering and not leaving bounds of the world map
+	google.maps.event.addListener(map, 'center_changed', function() {
+	    checkBounds(map);
+	});
+	// If the map position is out of range, move it back
+	function checkBounds(map) {
+
+		var latNorth = map.getBounds().getNorthEast().lat();
+		var latSouth = map.getBounds().getSouthWest().lat();
+
+		var newLat;
+
+        if(latNorth > 85.5)   {
+        	// console.log("n");
+            newLat =  map.getCenter().lat() - (latNorth-85);   /* too north, centering */
+        }
+        if(latSouth<-85.5){
+        	// console.log("s"); 
+            newLat =  map.getCenter().lat() - (latSouth+85);   /* too south, centering */
+		 }   
+		if(newLat) {
+		    var newCenter= new google.maps.LatLng( newLat ,map.getCenter().lng() );
+		    map.panTo(newCenter);
+		 }   
+	 }
+
+}
+
+function mainContentLoad() {
+	console.log("MAIN");
+	clearInterval(rotate);
+
+	// map
+	var map = document.getElementById("map");
+	map.style.filter = "";
+	
+	// textfield
+	var textField = document.getElementById("text-field");
+	var logo = document.getElementById("logo");
+	if (logo) {
+		textField.removeChild(logo);
+	}
+	textField.className = "content-loaded";
+	document.getElementById("input-wrapper").style.width = "auto";
+	var input = document.getElementById("input");
+	input.oninput = "textInput()"; // addEventListener merge with Helen's code 
+
+
+	// restrictions
+	document.getElementById("restrictions").style.display = "flex";
 	var rstPriceSlider = document.getElementById("rst-price-slider");
 	var rstPriceDisplayCurrent = document.getElementById("rst-price-display-current");
 	rstPriceDisplayCurrent.innerHTML = '$' + rstPriceSlider.value; // Display the default slider value
