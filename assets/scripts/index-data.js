6cb44edbbc8adf8b02f0b5a28f45b375aa12deff
Yang Yan <yangawesome@gmail.com>
2018-04-27 11:57:43 -0400

    animations, prefs, and data rebase

--- assets/scripts/index-data.js
+++ assets/scripts/index-data.js
@@ -3,5 +3,51 @@ var jsonData = {
         "What kind of weather are you looking for?",
         "What activities do you want to do?",
         "Any specific region in mind?"
+    ],
+    "data-markers": [
+        {
+            "lat": 27.775295,
+            "lng": -81.576485,
+            "name": "Florida",
+            "country": "United States",
+            "text": ["warm", "beach"],
+            "language": "---",
+            "location": "---",
+            "budget": "---",
+            "season": "summer",
+            "date": "Mar 19 - Mar 24",
+            "info": "Florida is the southernmost contiguous state in the United States. The state is bordered to the west by the Gulf of Mexico, to the northwest by Alabama, to the north by Georgia, to the east by the Atlantic Ocean, and to the south by the Straits of Florida.",
+            "link": "https://en.wikipedia.org/wiki/Florida"
+        },
+        {
+            "lat": 49.013454,
+            "lng": 12.100383,
+            "name": "Regensburg",
+            "country": "Germany",
+            "text": ["europe", "oldcities", "castles"],
+            "language": "german",
+            "location": "---",
+            "budget": "$$",
+            "season": "winter",
+            "date": "January 19 - January 23",
+            "info": "Regensburg is an old city in south-east Germany in Bavaria. It is located at the confluence of the Danube, Naab, and Regen rivers, " +
+                    + "and it is the political, economic,and cultural centre of easter Bavaria. The medieval centre of the city is UNESCO Worlk Heritage  Site, "
+                    + "and in 2014, it was among the top sights in Germany.",
+            "link": "https://en.wikipedia.org/wiki/Regensburg"
+        },
+        {
+            "lat": 37.593833,
+            "lng": -112.187092,
+            "name": "Bryce Canyon",
+            "country": "United States",
+            "text": ["hiking", "camping"],
+            "language": "---",
+            "location": "international",
+            "budget": "---",
+            "season": "summer",
+            "date": "June 7 - June 9",
+            "info": "Bryce Canyon National Park is a United States national park located in southwestern Utah. The major feature of the park is Bryce Canyon, which despite its name, is not a canyon, but a collection of giant natural amphitheaters along the eastern side of the Paunsaugunt Plateau. Bryce is distinctive due to geological structures called hoodoos, formed by frost weathering and stream erosion of the river and lake bed sedimentary rocks. The red, orange, and white colors of the rocks provide spectacular views for park visitors. Bryce sits at a much higher elevation than nearby Zion National Park. The rim at Bryce varies from 8,000 to 9,000 feet (2,400 to 2,700 m).",
+            "link": "https://en.wikipedia.org/wiki/Bryce_Canyon_National_Park"
+        },
     ]
 }
