c3b810f97491503a7488900cf99bd890d31da8c7
Yang Yan <yangawesome@gmail.com>
2018-05-06 23:11:59 -0400

    autocomplete

--- assets/scripts/index-main.js
+++ assets/scripts/index-main.js
@@ -1,3 +1,5 @@
+const cTagSuggestions = 4;
+
 let zoomBounds = [3, 10];
 
 var tfPhTime = 1500;
@@ -21,6 +23,8 @@ var inputWrapperMain;
 var tagSet;
 var mapMarkers = [];
 var gMapElement;
+var tagSuggestions = new Set([]);
+var acCurrentFocus;
 
 var unitedStates = false;
 
@@ -101,47 +105,166 @@ window.onload = function () {
 	};
 
 	navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-	
 }
 
-function initAutocomplete() {
+function acAddActive(x) {
+	/*a function to classify an item as "active":*/
+	if (!x) return false;
+	/*start by removing the "active" class on all items:*/
+	acRemoveActive(x);
+	if (acCurrentFocus >= x.length) acCurrentFocus = 0;
+	if (acCurrentFocus < 0) acCurrentFocus = (x.length - 1);
+	/*add class "autocomplete-active":*/
+	x[acCurrentFocus].classList.add("autocomplete-active");
+}
+function acRemoveActive(x) {
+	/*a function to remove the "active" class from all autocomplete items:*/
+	for (var i = 0; i < x.length; i++) {
+		x[i].classList.remove("autocomplete-active");
+	}
+}
+function acCloseAllLists(elmnt) {
+	/*close all autocomplete lists in the document,
+	except the one passed as an argument:*/
+	var x = document.getElementsByClassName("autocomplete-items");
+	for (var i = 0; i < x.length; i++) {
+		if (elmnt != x[i] && elmnt != textField) {
+			x[i].parentNode.removeChild(x[i]);
+		}
+	}
+}
 
+function acRefresh(elem) {
+	var a, b, i, val = elem.value;
+	/*close any already open lists of autocompleted values*/
+	acCloseAllLists();
+	if (!val) { return false; }
+	acCurrentFocus = -1;
+	/*create a DIV element that will contain the items (values):*/
+	a = document.createElement("DIV");
+	a.setAttribute("id", elem.id + "autocomplete-list");
+	a.setAttribute("class", "autocomplete-items");
+	/*append the DIV element as a child of the autocomplete container:*/
+	elem.parentNode.appendChild(a);
+	/*for each item in the array...*/
+	for (var item of tagSuggestions) {
+		//don't suggest anything that's already entered
+		if (tagSet.has(item))
+			continue;
+
+		//limit suggestions count
+		if (a.childElementCount >= cTagSuggestions)
+			break;
+
+		/*check if the item starts with the same letters as the text field value:*/
+		if (item.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+			/*create a DIV element for each matching element:*/
+			b = document.createElement("DIV");
+			/*make the matching letters bold:*/
+			b.innerHTML = "<strong>" + item.substr(0, val.length) + "</strong>";
+			b.innerHTML += item.substr(val.length);
+			/*insert a input field that will hold the current array item's value:*/
+			b.innerHTML += "<input type='hidden' value='" + item + "'>";
+			/*execute a function when someone clicks on the item value (DIV element):*/
+			b.addEventListener("click", function (e) {
+				/*insert the value for the autocomplete text field:*/
+				textField.value = this.getElementsByTagName("input")[0].value;
+				/*close the list of autocompleted values,
+				(or any other open lists of autocompleted values:*/
+				acCloseAllLists();
+
+				//simulate an enter
+				enterTag();
+				textField.focus();
+			});
+			a.appendChild(b);
+		}
+	}
+}
+
+//code from online to set up autocomplete, with some personal tweaks
+function initAutocomplete() {
+	for (var a = 0;a < jsonData['data-markers'].length;a++)
+		for (var b = 0;b < jsonData['data-markers'][a]['text'].length;b++)
+		tagSuggestions.add(jsonData['data-markers'][a]['text'][b]);
+
+	/*the autocomplete function takes two arguments,
+	the text field element and an array of possible autocompleted values:*/
+    /*execute a function when someone writes in the text field:*/
+	textField.addEventListener("input", function (e) {
+		acRefresh(this);
+	});
+    /*execute a function presses a key on the keyboard:*/
+    textField.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            acCurrentFocus++;
+            /*and and make the current item more visible:*/
+            acAddActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            acCurrentFocus--;
+            /*and and make the current item more visible:*/
+            acAddActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (acCurrentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[acCurrentFocus].click();
+            }
+        }
+    });
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        acCloseAllLists(e.target);
+    });
+}
+
+function enterTag() {
+	if (onLanding) {
+		endLanding();
+		onLanding = false;
+	}
+
+	var newTag = inputTagDummy.cloneNode(deep=true);
+	newTag.classList.remove('input-tag-hidden');
+	newTag.classList.add('input-tag-real');
+	newTag.id = '';
+	for (var a = 0;a < newTag.childNodes.length;a++) {
+		if (newTag.childNodes[a].className == 'input-tag-elem-text') {
+			newTag.childNodes[a].innerHTML = textField.value;
+			break;
+		}
+	}
+
+	tagSet.add(textField.value);
+	inputTags.appendChild(newTag);
+	textField.value = '';
+	
+	updateMap();
+	acRefresh(textField);
 }
 
 function initHandlers() {
 	textField.addEventListener('keydown', function(event) {
-        if (event.keyCode === 13 && textField.value != '' && !tagSet.has(textField.value)) {
-			if (onLanding) {
-				endLanding();
-				onLanding = false;
-			}
-
-			var newTag = inputTagDummy.cloneNode(deep=true);
-			newTag.classList.remove('input-tag-hidden');
-			newTag.classList.add('input-tag-real');
-			newTag.id = '';
-			for (var a = 0;a < newTag.childNodes.length;a++) {
-				if (newTag.childNodes[a].className == 'input-tag-elem-text') {
-					newTag.childNodes[a].innerHTML = textField.value;
-					break;
-				}
-			}
-
-			tagSet.add(textField.value);
-	        inputTags.appendChild(newTag);
-			textField.value = '';
-			
-	    	updateMap();
-		} else if (event.keyCode === 8 && textField.value == '') {
+        if (event.keyCode === 13 && textField.value != '' && !tagSet.has(textField.value))
+			enterTag();
+		else if (event.keyCode === 8 && textField.value == '') {
 			if (inputTags.lastChild.classList.contains('input-tag-real')) { //not dummy
 				event.preventDefault();
 				textField.value = getTagText(inputTags.lastChild);
 				removeTag(inputTags.lastChild);
+				acRefresh(textField);
 			}
     	}
 	});
 
-	inputTags.addEventListener('click', function(event){
+	inputTags.addEventListener('click', function(event) {
 		var tag = event.target;
 
 		//get correct ancestor
